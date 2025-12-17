58a504a1fc7ce951445cfb711a5ab61d5b9d1425
xmansx1 <xmansx1122@gmail.com>
2025-12-18 00:18:30 +0300

    Fix missing static files (icons.css, avatar-placeholder) and favicon route

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
     # لوحة الإدارة
     path("admin/", admin.site.urls),
     path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
